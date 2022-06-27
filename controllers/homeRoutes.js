2b6a3d0fd0d2edc9e5fdc945be5c21c4a77802c7
Kirsten Segavac <kirstensegavac@gmail.com>
2022-06-27 16:33:59 +0800

    added routes and server code

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,132 @@
+const router = require('express').Router();
+const { Blog, User } = require('../models');
+const withAuth = require('../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    // Get all blogs and JOIN with user and comment data
+    const blogData = await Blog.findAll({
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'blog_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['name'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const blogs = blogData.map((blog) => blog.get({ plain: true }));
+
+    // Pass serialized data and session flag into template
+    res.render('homepage', {
+      blogs,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/blog/:id', async (req, res) => {
+  try {
+    const blogData = await Blog.findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: ['id', 'title', 'created_at', 'content', 'user_id'],
+
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'blog_id',
+            'user_id',
+            'created_at',
+          ],
+          include: {
+            model: User,
+            attributes: ['name'],
+          },
+        },
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+    if (!blogData) {
+      res.status(404).json({ message: 'No blog found with this id' });
+      return;
+    }
+
+   // serialize the data
+   const blog = blogData.get({ plain: true });
+
+   // pass data to template
+   res.render('single-comment', {
+     blog,
+     loggedIn: req.session.loggedIn,
+   });
+ } catch (err) {
+   res.status(500).json(err);
+ }
+});
+
+
+// Use withAuth middleware to prevent access to route
+router.get('/blog', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Blog }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
+  res.render('login');
+});
+
+
+// If not already a user 
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+  res.render('signup');
+});
+
+module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const bcyrpt = require('bcrypt');
+const bcrypt = require('bcrypt');
 
 class User extends Model {
     checkPassword(loginPassword) {
